d3e9a5825df616fbc336c49aa23aeff760c054b2
Jen Kagan <jen@netlify.com>
2020-07-05 11:54:09 -0700

    test content-type checker

--- func/parse-media-type.go
+++ func/parse-media-type.go
@@ -2,22 +2,51 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"mime"
+	"strconv"
+	"strings"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
-	mediaType, params, err := mime.ParseMediaType(request.Headers["Content-Type"])
-	if err != nil {
-		log.Fatal(err)
+func splitMediaType(m string) (string, string) {
+	s := strings.SplitN(m, "/", 2)
+	if len(s) == 0 {
+		return "", ""
+	} else if len(s) == 1 {
+		return s[0], ""
 	}
-	fmt.Println("params: ", params)
+	return s[0], s[1]
+}
+
+func isBase64(r events.APIGatewayProxyRequest) bool {
+	completeMediaType, _, err := mime.ParseMediaType(r.Headers["content-type"])
+	if err != nil {
+		return false
+	}
+
+	mediaType, subType := splitMediaType(completeMediaType)
+	fmt.Println(mediaType)
+	fmt.Println(subType)
+	if mediaType == "text" {
+		return false
+	}
+	if mediaType == "application" {
+		return !(subType == "json" || strings.HasSuffix(subType, "+json")) &&
+			!(subType == "xml" || strings.HasSuffix(subType, "+xml")) &&
+			subType != "javascript"
+	}
+	return true
+}
+
+func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	var o = isBase64(request)
+
 	return &events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body:       string(mediaType),
+		Body: "isBase64: " + strconv.FormatBool(o) + "\n" +
+			"IsBase64Encoded: " + strconv.FormatBool(request.IsBase64Encoded) + "\n",
 	}, nil
 }
 
