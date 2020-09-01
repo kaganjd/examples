aa72970e41995551ed8b36aa2db45d59e8722800
Jen Kagan <jen@netlify.com>
2020-09-01 11:54:51 -0700

    test with json

--- func/vanilla.js
+++ func/vanilla.js
@@ -1,10 +1,11 @@
 exports.handler = async function(event, context) {
     const headerKey = "header value"
     const s = 200
-    const b = "this is a string!"
+    const b = {firstKey: "first value", secondKey: "second value"}
+    const j = JSON.stringify(b)
     return { 
-      headers: {headerKey},
+      headers: {headerKey: headerKey},
       statusCode: s, 
-      b
+      body: j
     };
 };
