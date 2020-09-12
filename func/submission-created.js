86cc3ed2674baf0aae771dc378015fe56003f804
Jen Kagan <jen@netlify.com>
2020-09-12 15:56:48 -0700

    test netlify js client  in a function

---
+++ func/submission-created.js
@@ -0,0 +1,37 @@
+const NetlifyAPI = require('netlify')
+
+exports.handler = async function (event, context) {
+  const client = new NetlifyAPI(process.env.NETLIFY_PAT)
+
+  const submissions = await client.listSiteSubmissions({
+      site_id: "628de7ef-04fb-4530-a7da-b795c0e5a457"
+    })
+    .catch((e) => console.log('Error getting submissions', e))
+
+  // async function asyncForEach(array, callback) {
+  //   for (let index = 0; index < array.length; index++) {
+  //     await callback(array[index], index, array);
+  //   }
+  // }
+
+  // const start = async () => {
+  //   console.log('inside Start')
+  //   await asyncForEach(submissions, async (submission) => {
+  //     console.log(submission.id)
+  //     await client.deleteSubmission({ submission_id: submission.id })
+  //     console.log('DELETED:', submission.id)
+  //   })
+  //   console.log('Finished deleting')
+  // }
+
+  if (submissions.length) {
+    // start()
+    for (i = 0; i < submissions.length; i++) {
+      console.log('starting to delete submission', submissions[i].id)
+      await client.deleteSubmission({ submission_id: submissions[i].id })
+      console.log('deleted submission', submissions[i].id)
+    }
+  } else {
+    console.log('NOTHING TO DELETE')
+  }
+}
