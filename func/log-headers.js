190cee8bfc7cc5593df7a544a4a7565631a2dd55
Jen Kagan <jen@netlify.com>
2020-11-23 10:04:02 -0800

    remove moldy functions demos, add a function to log request headers

---
+++ func/log-headers.js
@@ -0,0 +1,17 @@
+exports.handler = async function(event, context) {
+    console.log(event.headers)
+    const html = `
+        <!DOCTYPE html>
+        <html>
+        <head>
+        </head>
+            <body>
+                see <a href="https://app.netlify.com/sites/elastic-lalande-888212/functions/log-headers">function logs</a> for request headers
+            </body>
+        </html>
+    `
+    return {
+      statusCode: 200,
+      body: html
+    };
+};
