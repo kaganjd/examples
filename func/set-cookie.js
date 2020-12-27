1215eaad3c1dba2a1f81cb599eb70d78edad8d27
Jen Kagan <jen@netlify.com>
2020-12-26 20:19:44 -0800

    maybe this way?

--- func/set-cookie.js
+++ func/set-cookie.js
@@ -1,12 +1,8 @@
-const express = require("express");
-const serverless = require("serverless-http");
-
-const app = express();
-const router = express.Router();
-
-app.get("/.netlify/functions/set-cookie", (req, res) => {
-  return res
-    .cookie('rememberme', '1', { expires: new Date(Date.now() + 900000), httpOnly: true })
-})
-
-module.exports.handler = serverless(app);
+exports.handler = async function(event, context) {
+    const cookieVal = "boop"
+    console.log(cookieVal)
+    return {
+      statusCode: 200,
+      headers: {"set-cookie": "cookieName=cookieValue; HttpOnly"}
+    };
+};
