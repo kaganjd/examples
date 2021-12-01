b2d663fcdcabd34e1fdeab54ec223863ee3c7526
Jen Kagan <jen@netlify.com>
2021-12-01 08:19:45 -0800

    log event and context

--- func/query-string-params.js
+++ func/query-string-params.js
@@ -1,6 +1,8 @@
-exports.handler = async function (event) {
+exports.handler = async function (event, context) {
     const queryStringParams = event.queryStringParams
-    console.log(queryStringParams)
+    // console.log(queryStringParams)
+    console.log("event:", event)
+    console.log("context:",context)
   return {
     statusCode: 200,
     body: "🚀"
